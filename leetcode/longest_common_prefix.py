a28c3ddf550a9c1711b2897c0975fbbd8485787a
karthikpalavalli <karthikpalavalli@github.com>
2018-10-09 19:35:51 -0600

    longest common prefix

---
+++ leetcode/longest_common_prefix.py
@@ -0,0 +1,37 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        number_of_strs = len(strs)
+
+        if number_of_strs == 0:
+            return ""
+
+        elif number_of_strs == 1:
+            return strs[0]
+
+        else:
+            predicted_common_prefix = strs[0]
+
+            for i in range(1, len(strs)):
+                while True:
+                    if not predicted_common_prefix:
+                        return ""
+
+                    if predicted_common_prefix != strs[i][:len(predicted_common_prefix)]:
+                        predicted_common_prefix = predicted_common_prefix[:-1]
+
+                    else:
+                        break
+
+
+            return predicted_common_prefix
+
+
+if __name__ == "__main__":
+    test_strings = ["flower","flow","flight"]
+    lcp = Solution()
+    predicted_lcp = lcp.longestCommonPrefix(test_strings)
+    print(predicted_lcp)
