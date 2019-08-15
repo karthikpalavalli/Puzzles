2563f1359a8bab5ac8070143338a9b4eb979bc3e
karthikpalavalli <karthik00p@gmail.com>
2019-08-15 08:19:35 +0530

    [WIP] remove outermost parentheses

---
+++ leetcode/remove_outermost_parentheses.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeOuterParentheses(self, original_string: str) -> str:
+        parentheses_stack = list()
+
+        for ch in parentheses_stack:
+            if ch == '(':
+                print("WIP")
